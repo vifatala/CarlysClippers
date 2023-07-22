c7926d5de02d19b437da3533ca96495f7a7a5121
victorcasccavel <vifatala@gmail.com>
2023-07-22 12:30:32 -0300

    Submitting final project

---
+++ CarlysClippers.py
@@ -0,0 +1,38 @@
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+# Getting the total price and
+# getting the average price
+total_price = 0
+
+for price in prices:
+  total_price += price
+
+average_price = total_price / len(prices)
+print("Average Haircut Price:", average_price)
+print("------")
+
+# Cutting all prices by $5
+new_prices = [price - 5 for price in prices]
+print("Our new prices are:", new_prices)
+print("------")
+
+# Revenue
+total_revenue = 0
+
+for i in range(len(hairstyles)):
+  total_revenue = prices[i] * last_week[i]
+print("Total Revenue:", total_revenue)
+print("------")
+
+# Average Daily Revenue
+average_daily_revenue = total_revenue / 7
+print("Average Daily Revenue:", average_daily_revenue)
+print("------")
+
+# Advertisement
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices) - 1) if new_prices[i] < 30]
+print("Haircuts Under $30:", cuts_under_30)
